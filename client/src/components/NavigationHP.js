4ff8c662fde639f23d13c0cad3f832eeb3734808
Filiposis94 <79970294+Filiposis94@users.noreply.github.com>
2025-12-31 21:03:26 +0100

    fix: hp nav

--- client/src/components/NavigationHP.js
+++ client/src/components/NavigationHP.js
@@ -8,7 +8,7 @@ function NavigationHP(){
     return (
         <nav>   
         <div className="button hp-button" ><Link to="/freemarket"><div>Volní hráči<img className="hp-img" src={magGlass} alt="magnifying glass"></img></div></Link></div>
-        <div className="button hp-button" ><Link to="firendly-match"><div>Přáteláky<img className="hp-img" src={hockey} alt="hockey"></img></div></Link></div>
+        <div className="button hp-button" ><Link to="friendly-match"><div>Přáteláky<img className="hp-img" src={hockey} alt="hockey"></img></div></Link></div>
         <div className="button hp-button" ><Link to="tactics"><div>Taktiky<img className="hp-img" src={clipboard} alt="clipboard"></img></div></Link></div>
         <div className="button hp-button" ><Link to="visits"><div>Návštěvnost<img className="hp-img" src={people} alt="people"></img></div></Link></div>
         <div className="button hp-button" ><Link to="power"><div>Síly teamů<img className="hp-img" src={graph} alt="people"></img></div></Link></div>
